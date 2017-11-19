00aa1a27ff7b229c4b43800230a93e08a43825e1
DuPrince <744784681@qq.com>
2017-11-19 19:16:08 +0800

    Start MiUncleFrameWork Upload some folder and file

---
+++ Assets/Editor/MenuConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+//------------------------------------------------------------
+// MiUncleGameFramework v0.1a
+// Copyright (C) 2016-2017 MiUncle. All rights reserved.
+// Author: MiUncle
+// Date:2017/11/19
+//------------------------------------------------------------
+
+
+namespace MiUncle.Eidtor
+{
+    /// <summary>
+    /// 用来存放自定义拓展的编辑器的根节点
+    /// </summary>
+    public static class MenuConfig
+    {
+        public const string EDITOR = "GameEditor";//自定义通用的菜单都放在这个目录下
+    } 
+}
+
+
